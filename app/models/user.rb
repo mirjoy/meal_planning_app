855dd8ccead76846dd48ba0486bbcb02c72868fc
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-20 17:49:24 -0600

    created user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
 	has_secure_password
+
+	validates :user_name, presence: true, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,21 @@ require "support/factory_girl"
 
 RSpec.describe User, type: :model do
 	it "must have a name" do
-		user = create(:user, name: nil)
+		user = build(:user, user_name: nil)
+		user.save
+		expect(user).not_to be_valid
+	end
+
+	it "must have a unique name" do
+		user = create(:user, user_name: "nancy")
+		user2 = build(:user, user_name: "nancy")
+		user2.save
+		expect(user2).not_to be_valid
+	end
+
+	it "must have a password" do
+		user = build(:user, password: nil)
+		user.save
 		expect(user).not_to be_valid
 	end
 end
