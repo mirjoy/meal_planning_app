f595ab8840e51a34527409be0b34a6de881ebf79
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-21 13:35:17 -0600

    removed user validations that may interfere with facebook login

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,17 +7,4 @@ RSpec.describe User, type: :model do
 		user.save
 		expect(user).not_to be_valid
 	end
-
-	it "must have a unique name" do
-		user = create(:user, user_name: "nancy")
-		user2 = build(:user, user_name: "nancy")
-		user2.save
-		expect(user2).not_to be_valid
-	end
-
-	it "must have a password" do
-		user = build(:user, password: nil)
-		user.save
-		expect(user).not_to be_valid
-	end
 end
