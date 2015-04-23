24365b76c399c185731eae71e199477214a1a620
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-23 10:26:36 -0600

    generated mailers

---
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,14 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_confirmation
+  def account_confirmation
+    UserMailer.account_confirmation
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  def password_reset
+    UserMailer.password_reset
+  end
+
+end
