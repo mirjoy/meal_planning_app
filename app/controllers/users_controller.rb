9deeb6d31355619d115e15a27fcad91b8e24d80f
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-23 09:37:56 -0600

    updated view of meal planning page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
 
   def show
   	if !current_user
-  		flash[:danger] = "You are not authorized to access this page"
-  		redirect_to root_path
+      flash[:danger] = "You are not authorized to access this page"
+      redirect_to root_path
   	end
   end
 
