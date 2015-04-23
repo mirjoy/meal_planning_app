ef83f09e78aa286f90fc05f836126c81fd495be5
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-23 11:45:24 -0600

    added the password reset form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422205347) do
+ActiveRecord::Schema.define(version: 20150423173655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150422205347) do
     t.string   "token"
     t.string   "expires_at"
     t.string   "password_digest"
+    t.string   "reset_digest"
   end
 
 end
