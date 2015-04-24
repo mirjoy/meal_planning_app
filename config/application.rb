a9866f2ac4dd1743f57ec3dd4c04f7c4339da3bc
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-23 21:52:52 -0600

    moved SendGrid method to environment per their documentation

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,5 @@ module Delistcious
 
     config.action_mailer.delivery_method = :smtp
 
-    config.action_mailer.smtp_settings = {
-		  :user_name => ENV['sendgrid_name'],
-		  :password => ENV['sendgrid_pw'],
-		  :domain => "localhost:3000",
-		  :address => 'smtp.sendgrid.net',
-		  :port => 587,
-		  :authentication => :plain,
-		  :enable_starttls_auto => true
-		}
   end
 end
