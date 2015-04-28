da276d7da6c75f0437b95c73c035a7d771c816c2
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-28 15:49:56 -0600

    added down arrow to toggle button

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,8 +6,5 @@ class MealsController < ApplicationController
 		@banned_foods = current_user.banned_foods
 		@allergies = Allergy.all
 		@diets = Diet.all
-	
-		# yummly allergies = %w(Dairy Egg Gluten Peanut Seafood Sesame Soy Sulfite Tree Nut Wheat)
-		# yummly diets = %w(Lacto-vegetarian Ovo-vegetarian Pescetarian Vegan Vegetarian)
 	end
 end
