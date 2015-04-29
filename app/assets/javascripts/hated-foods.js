13e714b28da2b5aaa14b6619d82067bf14b0a041
Miriam Moser <mirjoy.moser@gmail.com>
2015-04-28 20:10:00 -0600

    made hound changes

--- app/assets/javascripts/hated-foods.js
+++ app/assets/javascripts/hated-foods.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
-  toggleFoodList('#specific-food-header', '#specific-food');
-  toggleFoodList('#allergy-header', '#allergy');
-  toggleFoodList('#diet-header', '#diet');
+  toggleFoodList("#specific-food-header", "#specific-food");
+  toggleFoodList("#allergy-header", "#allergy");
+  toggleFoodList("#diet-header", "#diet");
 
   function toggleFoodList(foodCategory, foodList){
-    $(foodCategory).bind('click', function(){
-      if ($(foodList).hasClass('hidden')){
-        $('.hated-food').addClass('hidden');
+    $(foodCategory).bind("click", function(){
+      if ($(foodList).hasClass("hidden")){
+        $(".hated-food").addClass("hidden");
       }
       else {
-        $('.hated-food').addClass('hidden');
-        $(foodList).removeClass('hidden');
+        $(".hated-food").addClass("hidden");
+        $(foodList).removeClass("hidden");
       }
-      $(foodList).toggleClass('hidden');
+      $(foodList).toggleClass("hidden");
     });
   };
 
-  $('.hated-food').find('button').on('click', function(){
-    $(this).toggleClass('btn-danger');
+  $(".hated-food").find("button").on("click", function(){
+    $(this).toggleClass("btn-danger");
   });
 
-  $("#favorite-number").bind('click', function(){
-    $("#favorite-number-selection").toggleClass('hidden')
+  $("#favorite-number").bind("click", function(){
+    $("#favorite-number-selection").toggleClass("hidden")
   });
 
   $("#specific-food").on("ajax:success", function(e, data, status, xhr) {
-      var button = $('<button type="button" class="btn btn-danger one-banned-food" style="margin: 2px" data-id=' + data.id + '>'+data.name+'</button>');
+      var button = $("<button type='button' class='btn btn-danger one-banned-food' style='margin: 2px' data-id=" + data.id + ">"+data.name+"</button>");
       $("#banned-food-list").append(button);
       return $("input").val("")
     }).on("ajax:error", function(e, xhr, status, error) {
@@ -33,7 +33,7 @@ $(document).ready(function(){
     return $("#banned-food-list").append("<p>You have already banned that food.</p>");
   });
 
-   $('.one-banned-food').on('click', function(e){
+   $(".one-banned-food").on("click", function(e){
       var food_id = $(this).attr("data-id");
       
       $.ajax({
