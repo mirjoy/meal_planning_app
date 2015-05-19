93aa6c3d3642128606f8790a780cbec777aa1759
Miriam Moser <mirjoy.moser@gmail.com>
2015-05-18 20:11:03 -0600

    updated sendgrid keys to work with heroku

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings  = {
-  user_name: 						ENV['sendgrid_name'],
-  password: 						ENV['sendgrid_pw'],
+  user_name: 						ENV['SENDGRID_USERNAME'],
+  password: 						ENV['SENDGRID_PASSWORD'],
   domain: 							"localhost:3000",
   address: 							"smtp.sendgrid.net",
   port:  								587,
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
+# always ask for authorization so i can find the pain point
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :facebook, ENV['facebook_key'], ENV['facebook_secret']	
 end
